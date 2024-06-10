4ae8b204a7ff4c219f2c8f8dacb491315ab73bca
PatrickRodrigues19 <patrick.rodrigues@sptech.school>
2024-06-10 17:20:02 -0300

    Alteração

--- docker.sh
+++ docker.sh
@@ -3,5 +3,4 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y docker.io docker-compose
 sudo systemctl start docker && sudo systemctl enable docker
-git clone https://github.com/conecta-sptech/Docker.git
 sudo docker-compose up
