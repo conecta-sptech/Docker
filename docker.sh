ced69f7b551dd5a3b597c76072f9d55637ab774d
PatrickRodrigues19 <patrick.rodrigues@sptech.school>
2024-06-10 20:01:36 -0300

    Alteração

--- docker.sh
+++ docker.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y docker.io docker-compose
-sudo systemctl start docker && sudo systemctl enable docker
+sudo apt update
+sudo apt upgrade -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 sudo docker-compose up
+
